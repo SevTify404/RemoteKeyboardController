54d3b10891d9a0b4be475814c697fd0115ddcb35
Barry Ali <pavelnemesis288@gmail.com>
2025-12-14 12:07:01 +0000

    back-feat/secu: Mise en place de la logique derrier challenge et PIN (code natif pure sans fastApi)

---
+++ backend/app/utils/security/pin_manager.py
@@ -0,0 +1,73 @@
+from datetime import datetime, timedelta
+import secrets
+import hmac
+
+from typing import Union
+from uuid import UUID
+
+from app.schemas.security_schema import PinSchema
+
+
+class PinManager:
+    """class pour gerer les oepration sur le PIN"""
+
+    def __init__(self, time_to_live: int = 5):
+        self.ttl_minutes = time_to_live
+        self._pins: dict[UUID, PinSchema] = {}
+
+
+    def get_pin(self, challenge_id: UUID) -> Union[PinSchema, None]:
+      """funct pour lire un PIN generer"""
+
+      pin = self._pins.get(challenge_id)
+      return pin
+    
+    
+    def create_pin(self, challenge_id) -> PinSchema:
+      """funct pour creer un PIN"""
+
+      created_pin = PinSchema(
+        challenge_id=challenge_id,
+        pin_code=f"{secrets.randbelow(1_000_000):06}",
+        created_at=datetime.now(),
+        expires_at=datetime.now() + timedelta(minutes=self.ttl_minutes)
+      )
+      
+      self._pins[created_pin.challenge_id] = created_pin
+      
+      return created_pin
+      
+      
+      
+    def is_valid_pin(self, challenge_id: UUID, given_pin: str) -> bool:
+      """funct pr verifer la validiter d'un PIN/et aussi le blocquer si 
+      trop de tentative"""
+
+      pin = self.get_pin(challenge_id)
+      
+      if not pin or pin.used:
+        return False
+      
+      if pin.expires_at < datetime.now():
+        return False
+      
+      if not hmac.compare_digest(given_pin, pin.pin_code):
+        pin.attempts += 1
+        if pin.attempts > pin.max_attempts:
+          pin.blocked = True
+        
+        return False
+      
+      return True
+    
+    
+    def mark_pin_as_used(self, challenge_id: UUID) -> None:
+      """funct pour marquer un pin comme deja utiliser"""
+
+      pin = self.get_pin(challenge_id)
+
+      if pin: 
+        pin.used = True
+        
+      return None
+        
