c5d32079ecac21f6732b5c8ff79cd5221e4eff8c
SevTify[404] <gametech228@gmail.com>
2025-12-14 01:35:34 +0000

    refactor: Reorganisation des fichiers

--- backend/app/services/master_ws/websocket_conn_manager.py
+++ backend/app/services/master_ws/websocket_conn_manager.py
@@ -1,53 +1,9 @@
-from dataclasses import dataclass
-from enum import Enum
-from typing import Optional, Any
+from typing import Any
 from fastapi import WebSocket, WebSocketDisconnect
 
-@dataclass
-class AvailableWebSocketScopes:
-    """Classe pour définir les scopes WebSocket disponibles dans l'application"""
-    admin_side: Optional[WebSocket] = None
-    client_side: Optional[WebSocket] = None
-    waiting_for_connection_side: Optional[WebSocket] = None
+from app.services.master_ws.aliases import SideAlias
+from app.services.master_ws.scopes import AvailableWebSocketScopes
 
-    @property
-    def is_admin_connected(self) -> bool:
-        """Vérifie si le côté admin est connecté"""
-        return self.admin_side is not None
-
-    @property
-    def is_client_connected(self) -> bool:
-        """Vérifie si un client est connecté"""
-        return self.client_side is not None
-
-    @property
-    def is_waiting_for_connection(self) -> bool:
-        """Vérifie si on est en attente d'une connexion"""
-        return self.waiting_for_connection_side is not None
-
-    def remove_user_connection(self):
-        """Supprime la connexion du client"""
-        self.client_side = None
-
-    def remove_admin_connection(self):
-        """Supprime la connexion du côté admin"""
-        self.admin_side = None
-
-    def remove_waiting_for_connection(self):
-        """Supprime la connexion en attente"""
-        self.waiting_for_connection_side = None
-
-
-    def reset(self):
-        """Réinitialise toutes les connexions WebSocket"""
-        self.admin_side = None
-        self.client_side = None
-        self.waiting_for_connection_side = None
-
-class SideAlias(str, Enum):
-    ADMIN_SIDE = "admin"
-    CLIENT_SIDE = "client"
-    WAITING_FOR_CONNECTION_SIDE = "waiting"
 
 class AppWebSocketConnectionManager:
     """Classe Sinleton pour gérer les connexions WebSocket dans l'application."""
