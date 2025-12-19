14bf3fc8f81cbc7024c43afa8a8714097c0f4528
SevTify[404] <gametech228@gmail.com>
2025-12-19 08:29:41 +0000

    refactor: Modification de la logique de gestion des combinaisons de touches de clavier. Mise à jour de `availables.py` pour intégrer des combinaisons de touches courantes facilitant ainsi l'interaction avec le contrôleur de clavier.

--- backend/app/services/keyboard_controller/availables.py
+++ backend/app/services/keyboard_controller/availables.py
@@ -1,9 +1,10 @@
 from dataclasses import dataclass
 from enum import Enum
 
-from pynput.keyboard import Key
+from pynput.keyboard import Key, KeyCode
 
-from app.services.keyboard_controller._custom_touchs import KeyboardTouchs, SingleKeyTouch
+from app.services.keyboard_controller._custom_touchs import KeyboardTouchs, SingleKeyTouch, KeyboardCombination, \
+    KeyboardCombinationTouch
 
 
 class AvailableKeys(str, Enum):
@@ -16,6 +17,15 @@ class AvailableKeys(str, Enum):
     VOLUME_UP_KEY="VOLUME_UP"
     VOLUME_DOWN_KEY="VOLUME_DOWN"
 
+
+    # Les combos
+    COPY = "COPY"
+    PASTE = "PASTE"
+    SELECT_ALL = "SELECT_ALL"
+    ALT_TAB = "ALT_TAB"
+    START_PRESENTATION = "START_PRESENTATION"
+    END_PRESENTATION = "END_PRESENTATION"
+
 @dataclass
 class KeysImplementations:
     UP_KEY = SingleKeyTouch(Key.up)
@@ -27,6 +37,50 @@ class KeysImplementations:
     VOLUME_UP_KEY = SingleKeyTouch(Key.media_volume_up)
     VOLUME_DOWN_KEY = SingleKeyTouch(Key.media_volume_down)
 
+    #Les combos
+    COPY = KeyboardCombinationTouch(
+        KeyboardCombination(
+            keys_to_hold=[Key.ctrl],
+            keys_to_press=[KeyCode(char='c')]
+        )
+    )
+
+    PASTE = KeyboardCombinationTouch(
+        KeyboardCombination(
+            keys_to_hold=[Key.ctrl],
+            keys_to_press=[KeyCode(char='v')]
+        )
+    )
+
+    SELECT_ALL = KeyboardCombinationTouch(
+        KeyboardCombination(
+            keys_to_hold=[Key.ctrl],
+            keys_to_press=[KeyCode(char='a')]
+        )
+    )
+
+    ALT_TAB = KeyboardCombinationTouch(
+        KeyboardCombination(
+            keys_to_hold=[Key.alt],
+            keys_to_press=[Key.tab]
+        )
+    )
+
+    START_PRESENTATION = KeyboardCombinationTouch(
+        KeyboardCombination(
+            keys_to_hold=[Key.f5],
+            keys_to_press=[]
+        )
+    )
+
+    END_PRESENTATION = KeyboardCombinationTouch(
+        KeyboardCombination(
+            keys_to_hold=[Key.esc],
+            keys_to_press=[]
+        )
+    )
+
+
 
 key_map :  dict[AvailableKeys, KeyboardTouchs] = {
     AvailableKeys.UP_KEY : KeysImplementations.UP_KEY,
@@ -36,5 +90,14 @@ key_map :  dict[AvailableKeys, KeyboardTouchs] = {
     AvailableKeys.ENTER_KEY : KeysImplementations.ENTER_KEY,
     AvailableKeys.MUTE_KEY : KeysImplementations.MUTE_KEY,
     AvailableKeys.VOLUME_UP_KEY : KeysImplementations.VOLUME_UP_KEY,
-    AvailableKeys.VOLUME_DOWN_KEY : KeysImplementations.VOLUME_DOWN_KEY
+    AvailableKeys.VOLUME_DOWN_KEY : KeysImplementations.VOLUME_DOWN_KEY,
+
+    # Les combos
+    AvailableKeys.COPY: KeysImplementations.COPY,
+    AvailableKeys.PASTE: KeysImplementations.PASTE,
+    AvailableKeys.SELECT_ALL: KeysImplementations.SELECT_ALL,
+    AvailableKeys.ALT_TAB: KeysImplementations.ALT_TAB,
+    AvailableKeys.START_PRESENTATION: KeysImplementations.START_PRESENTATION,
+    AvailableKeys.END_PRESENTATION: KeysImplementations.END_PRESENTATION,
+
 }
