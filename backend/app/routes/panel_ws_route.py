e67df92d8f1181bca37fae4528294cd8d5c4aa12
SevTify[404] <gametech228@gmail.com>
2025-12-19 23:09:58 +0000

    fix: Mise à jour de la gestion des connexions WebSocket pour le panneau d'administration
    Cette modification remplace l'importation de `app_websocket_connection_manager` par `app_websocket_manager` dans le fichier `panel_ws_route.py`.

--- backend/app/routes/panel_ws_route.py
+++ backend/app/routes/panel_ws_route.py
@@ -1,17 +1,17 @@
 from fastapi import WebSocket, WebSocketDisconnect
 from app.routes.ws_router import router
-from app.services.master_ws.websocket_conn_manager import app_websocket_connection_manager
+from app.services import app_websocket_manager
 
 
 @router.websocket("/ws/panel")
 async def panel_websocket(websocket: WebSocket):
   
-  await app_websocket_connection_manager.connect_admin(websocket)
+  await app_websocket_manager.connect_admin(websocket)
   try:
     while True:
       
       await websocket.receive_json()
       
   except WebSocketDisconnect:
-    await app_websocket_connection_manager.disconnect_admin("Le coté Admin Panel s;est déconnecter") 
+    await app_websocket_manager.disconnect_admin("Le coté Admin Panel s;est déconnecter")
 
