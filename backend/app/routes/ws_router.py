dcf8c948bca10d00479b7ddabf75d9a70e5d27a7
SevTify[404] <gametech228@gmail.com>
2025-12-26 19:48:43 +0000

    Ajou des import pour re-rendre le code fonctionnel

--- backend/app/routes/ws_router.py
+++ backend/app/routes/ws_router.py
@@ -4,6 +4,8 @@ router = APIRouter(prefix="/ws", tags=["WebSocket Routes"])
 
 # Petite fonction inutile (mais utile) pour ajouter les routes websocket à l'objet sinon on aura que des 403
 def add_ws_routes():
-    pass
+    from .admin_panel_ws_route import panel_websocket
+    from .waiting_ws_route import waiting_connexion
+    from .control_panel_ws_route import control_panel_websocket
 
 add_ws_routes()
